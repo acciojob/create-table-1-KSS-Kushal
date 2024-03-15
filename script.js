bca49bf42504aa64314e3e9b4033a0a1ad7af865
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-15 21:02:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+  const add = `
+  <tr>
+  <td>New Cell1</td>
+  <td>New Cell2</td>
+  </tr>
+  `;
+	const tableHTML = document.getElementById("sampleTable");
+	tableHTML.innerHTML = add + tableHTML.innerHTML;
   
 }
