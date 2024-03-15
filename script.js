738fa8d4d34bf0bf8bbd0397b870155e65ef75ae
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-15 21:09:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function insert_Row() {
     //Write your code here
-  const add = `
-  <tr>
-  <td>New Cell1</td>
-  <td>New Cell2</td>
-  </tr>
-  `;
 	const tableHTML = document.getElementById("sampleTable");
-	tableHTML.innerHTML = add + tableHTML.innerHTML;
-  
+	 var row = tableHTML.insertRow(0);
+    
+    // Insert new cells (<td>) into the row
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    
+    // Add some text to the new cells
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
 }
